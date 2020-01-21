6d87fea9646cb230456d5e02657596f9f0152095
Alex Pritchard <alexpritc@users.noreply.github.com>
2020-01-21 22:21:37 +0000

    Added jumping, panning camera and lighthouse.

---
+++ Lighthouse Game/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject cameraPosLeft;
+    public GameObject cameraPosRight;
+    public GameObject cameraPosCenter;
+
+    public GameObject cam;
+    public GameObject left;
+    public GameObject right;
+
+    private bool isMoving = false;
+
+    private Vector3 newCamPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam.transform.position = cameraPosCenter.transform.position;
+        cam.transform.rotation = cameraPosCenter.transform.rotation;
+    }
+
+    private void Update()
+    {
+        if (isMoving) 
+        {
+            Move(newCamPos);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == left)
+        {
+            if (transform.position.x <= left.transform.position.x)
+            {
+                newCamPos = cameraPosLeft.transform.position;
+            }
+            else 
+            {
+                newCamPos = cameraPosCenter.transform.position;
+            }
+
+            isMoving = true;
+        }
+        else if (other.gameObject == right)
+        {
+            if (transform.position.x >= right.transform.position.x)
+            {
+                newCamPos = cameraPosRight.transform.position;
+            }
+            else
+            {
+                newCamPos = cameraPosCenter.transform.position;
+            }
+
+            isMoving = true;
+        }
+    }
+
+    void Move(Vector3 newPos) 
+    {
+        cam.transform.position = Vector3.Lerp(cam.transform.position, newPos, 0.01f);        
+
+        if (cam.transform.position == newPos) 
+        {
+            isMoving = false;
+        }
+    }
+}
--- Lighthouse Game/Assets/Scripts/PlayerMovement.cs
+++ Lighthouse Game/Assets/Scripts/PlayerMovement.cs
@@ -4,41 +4,78 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float speed = 2.5f;
+    private float speed = 3.33f;
+    private float distanceToGround = 0.6f;
+    private float gravity = -19.7f;
+    private float jumpHeight = 1f;
 
-    private float jumpSpeed = 10f;
-    private float gravity = 10f;
-    private float velocity = 10f;
-
+    private Vector3 velocity;
+    private Vector3 down;
     private Vector3 move;
 
-    private Rigidbody2D rb;
+    private CharacterController cc;
 
-    // Start is called before the first frame update
+    private bool isGrounded;
+
+    private RaycastHit hit;
+
+    private Transform playerTransform;
+
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();
+        cc = gameObject.GetComponent<CharacterController>();
+
+        playerTransform = transform;
+        isGrounded = true;
     }
 
-    // Update is called once per frame
+    // Update is called once per frame.
+    // Player input.
     void Update()
     {
-        Movement();
-    }
+        // Get jump input.
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
+            //Debug.Log("Jump!");
 
-    void Movement() 
-    {
+            isGrounded = false;
+        }
+
+        // Get movement input.
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        // Local movement.
-        move = transform.right * x + transform.forward * z;
+        // Local movement according to player's rotation.
+        move = playerTransform.right * x + playerTransform.forward * z;
 
-        transform.position += move * speed * Time.deltaTime;
+        // Apply gravity to velocity.
+        velocity.y += gravity * Time.deltaTime;
     }
 
-    void Jump() 
+    // Physics.
+    void FixedUpdate()
     {
-        
+        // Play footstep sounds here.
+        cc.Move(move * speed * Time.deltaTime);
+
+        // Play jump sound here.
+        cc.Move(velocity * Time.deltaTime);
+        GroundCheck();
+    }
+
+    void GroundCheck()
+    {
+        // Draw raycast downwards to check if the player is touching the ground.
+        down = transform.TransformDirection(Vector3.down);
+
+        //Debug.DrawRay(transform.position, down, Color.red);
+
+        if (Physics.Raycast(transform.position, down, out hit, distanceToGround))
+        {
+            Debug.DrawRay(transform.position, down * hit.distance, Color.yellow);
+            isGrounded = true;
+            velocity.y = 0f;
+        }
     }
 }
